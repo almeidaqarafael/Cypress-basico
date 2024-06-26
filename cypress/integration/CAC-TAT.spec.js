c2cb39b35ff0522214fe5040ee926baf98679cdc
almeidaqarafael <rafaeltesterqa@gmail.com>
2024-06-26 10:33:20 -0300

    Adicionando casos de testes com select

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,7 +5,7 @@ describe('Central de Atendimento ao Cliente - TAT', () => {
   beforeEach(() => {
     cy.visit('./src/index.html');
 
-  })
+  });
   
   it('verifica o título da aplicação', () => {
 
@@ -126,5 +126,23 @@ describe('Central de Atendimento ao Cliente - TAT', () => {
   it('envia o formuário com sucesso usando um comando customizado', () => {
     cy.enviaFormulario();
   });
+
+  it('seleciona um produto (YouTube) por seu texto', () => {
+    cy.get('#product')
+      .select('YouTube')
+      .should('have.value', 'youtube');
+  }); 
+
+  it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+    cy.get('#product')
+      .select('mentoria')
+      .should('have.value', 'mentoria');
+  });
+
+  it('seleciona um produto (Blog) por seu índice', () => {
+    cy.get('#product')
+      .select(1)
+      .should('have.value', 'blog');
+  });  
 
 })
